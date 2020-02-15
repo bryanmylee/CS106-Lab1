37ea2da05588d9cf6928b79b45510f95476eab56
Bryan Lee <bryanleeminyuan@gmail.com>
2020-02-15 17:55:39 +0800

    Implement rotation tracking
    Additional changes
    * Check verticality with |<z>| instead of |<x, y>|
    * Place all orientation tracking functionality within OrientationManager
      class
    * Separate vertical and horizontal functionality into separate classes

--- source/main.cpp
+++ source/main.cpp
@@ -40,7 +40,7 @@ void countdown(int start) {
     }
 }
 
-void aTimeForEverything() {
+void timeForEverything() {
     ExtendedButton buttonA = ExtendedButton(&uBit.buttonA, 500);
     ExtendedButton buttonB = ExtendedButton(&uBit.buttonB, 500);
 
@@ -70,66 +70,121 @@ void aTimeForEverything() {
  *   0   : perfectly horizontal
  *   1000: perfectly vertical
  */
-#define VERT_MARGIN 750
+#define HORIZONTAL_MARGIN 600
 // Defines how many consecutive clean data points are captured before we register a change in verticality
-#define VERT_SENS 20
+#define ORIENTATION_SENS 20
+#define SQRT3 1.7320508
 
 enum Orientation { HORIZONTAL = 0, VERTICAL = 1 };
 
-int getSquareMagnitude(int x, int y) {
-    return x * x + y * y;
+int abs(int x) {
+    return x >= 0 ? x : -x;
 }
 
-/* 
- * We can check for orientation by checking the magnitude of the vector <x, y>
- *   0   : perfectly horizontal
- *   1000: perfectly vertical
- */
-Orientation getOrientationRaw() {
-    if (getSquareMagnitude(uBit.accelerometer.getX(), uBit.accelerometer.getY()) > VERT_MARGIN * VERT_MARGIN) {
-        return VERTICAL;
-    }
-    return HORIZONTAL;
+double arctan(double x) {
+    if (x < 0) return -arctan(-x);
+    if (x > 1) return PI / 2 - arctan(1 / x);
+    if (x > 2 - SQRT3) return PI / 6 + arctan((SQRT3 * x - 1) / (SQRT3 + x));
+    return x - x * x * x / 3 + x * x * x * x * x / 5;
 }
 
-/*
- * Wait until we get VERT_SENS data points before registering a change in orientation
- *
- * @param (*onChange)() callback function when orientation changes
- */
-Orientation orientationReal = HORIZONTAL;
-int changedCount = 0;
-Orientation getOrientationBuffered(void (*onChange)()) {
-    if (getOrientationRaw() ^ orientationReal) {
-        changedCount++;
-    } else {
-        changedCount = 0;
-        return orientationReal;
+double getRadians(int x, int y) {
+    /*
+     * If x ~= 0, then y / x becomes very large.
+     * To avoid that, we take the smaller ratio, and offset from PI / 2 instead.
+     */
+    if (y >= 0) {
+        if (x >= y) {
+            return arctan((double) y / x);
+        }
+        return PI / 2 - arctan((double) x / y);
     }
-    if (changedCount > VERT_SENS) {
-        orientationReal = (Orientation) !orientationReal;
-        (*onChange)();
-    }
-    return orientationReal;
+    // tan has a periodicity of PI, so we have to offset by PI to get the full 2PI radians.
+    return PI + getRadians(-x, -y);
 }
 
-Orientation getOrientationBuffered() {
-    return getOrientationBuffered([](){});
+double getDegrees(int x, int y) {
+    return getRadians(x, y) / PI * 180;
 }
+
+class OrientationManager {
+    private:
+        Orientation orientationReal = HORIZONTAL;
+        int changedCount = 0;
+
+        /* 
+         * We can check for orientation by checking the magnitude of the vector <z>
+         *      0: perfectly vertical
+         *   1023: perfectly horizontal
+         */
+        Orientation getOrientationRaw() {
+            if (abs(uBit.accelerometer.getZ()) > HORIZONTAL_MARGIN) {
+                return HORIZONTAL;
+            }
+            return VERTICAL;
+        }
+    public:
+        /*
+         * Wait until we get VERT_SENS data points before registering a change in orientation
+         *
+         * @param (*onChange)() callback function when orientation changes
+         */
+        Orientation getOrientationBuffered(void (*onChange)()) {
+            if (getOrientationRaw() ^ orientationReal) {
+                changedCount++;
+            } else {
+                changedCount = 0;
+                return orientationReal;
+            }
+            if (changedCount > ORIENTATION_SENS) {
+                orientationReal = (Orientation) !orientationReal;
+                (*onChange)();
+            }
+            return orientationReal;
+        }
+
+        Orientation getOrientationBuffered() {
+            return getOrientationBuffered([](){});
+        }
+} orientationManager;
+
+class VerticalParadox {
+    public:
+        void run() {
+            int degrees = (int) getDegrees(uBit.accelerometer.getX(), uBit.accelerometer.getY());
+            int modDegrees = degrees % 360;
+            uBit.serial.send(modDegrees);
+            uBit.serial.send("\r\n");
+        }
+
+        void reset() {
+        }
+} vertParadox;
+
+class HorizontalParadox {
+    public:
+        void run() {
+            uBit.serial.send("run hori\r\n");
+        }
+
+        void reset() {
+        }
+} horiParadox;
 
 void onOrientationChange() {
     uBit.display.printChar(' ');
+    vertParadox.reset();
+    horiParadox.reset();
 }
 
 void paradoxThatDrivesUsAll() {
     while (1) {
-        // uBit.serial.send("x: " + ManagedString(uBit.accelerometer.getX()) + "|");
-        // uBit.serial.send("y: " + ManagedString(uBit.accelerometer.getY()) + "|");
-        // uBit.serial.send("z: " + ManagedString(uBit.accelerometer.getZ()) + "\r\n");
-
-        Orientation currOrient = getOrientationBuffered(&onOrientationChange);
-        uBit.serial.send(currOrient);
-        uBit.serial.send("\r\n");
+        Orientation currOrient = orientationManager.getOrientationBuffered(&onOrientationChange);
+        if (currOrient == VERTICAL) {
+            vertParadox.run();
+        } else {
+            horiParadox.run();
+        }
     }
 }
 
